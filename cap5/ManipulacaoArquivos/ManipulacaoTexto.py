26bdeeb93e6b044b87cfdcbfb2717f68e0db6be3
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-11-04 02:33:22 -0300

    Strings! Listas de novo
    trabalho string como lista

---
+++ cap5/ManipulacaoArquivos/ManipulacaoTexto.py
@@ -0,0 +1,8 @@
+texto = "Um texto simples"
+        #1234567890123456
+
+print("index positivo: ", texto[:8])
+print("index negativo: ", texto[-16:-8])
+print("Inverter texto: ", texto[::-1])
+
+print("Pular textos: ", texto[0::3])
